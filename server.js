3d7ed75519f6b17322192d5a190e2b356d556274
RaynoldTanYX <raynoldtanyx@gmail.com>
2021-03-03 14:38:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ db.once('open', () => console.log("Connected to Database"));
 
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const recipesRouter = require('./routes/recipes');
 app.use('/recipes', recipesRouter);
