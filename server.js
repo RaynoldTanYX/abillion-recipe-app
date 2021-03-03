eb6f4241ffb44163540ed14aab10f78a5342dc00
RaynoldTanYX <raynoldtanyx@gmail.com>
2021-03-03 15:27:06 +0800

    Added port number to env

--- server.js
+++ server.js
@@ -12,29 +12,30 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Connected to Database"));
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: '*'}));
 // Add headers
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 const recipesRouter = require('./routes/recipes');
 app.use('/recipes', recipesRouter);
 
-app.listen(3000, () => console.log("Server started"));
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log("Server started on " + port));
 
