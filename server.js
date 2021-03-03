2d5df2429bb3d64f69e96d6b56d59f2d9b73117c
RaynoldTanYX <raynoldtanyx@gmail.com>
2021-03-03 14:48:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,25 @@ db.once('open', () => console.log("Connected to Database"));
 
 app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-  });
+});
 
 const recipesRouter = require('./routes/recipes');
 app.use('/recipes', recipesRouter);
